3c0bd00fa6b83d851ca142c2f9af7fe7d550d38c
hedgehog0912 <eu.salnikoff2017@yandex.ru>
2022-08-26 19:05:06 +0300

    added new change

--- TestLaunch.java
+++ TestLaunch.java
@@ -1,5 +1,5 @@
 public class TestLaunch {
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        System.out.println("Hello github + change 1");
     }
 }
